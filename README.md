9b15489c706da961dc60a9049c291271b5275820
yoon1 <o_ink@naver.com>
2021-07-28 22:55:08 +0900

    폴더 구조 변경
    언어별 > 플랫폼별

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # ***알고리즘 문제 해결*** 
-
-Language : C++  
-문제 출처 : baekjoon, SWAcademy, Programmers
+Language : C++, Python, Java
+문제 출처 : Baekjoon, SWAcademy, Programmers, LeetCode
--- cpp/PROGRAMMERS/LV3_입국심사.cpp
+++ cpp/PROGRAMMERS/LV3_입국심사.cpp
@@ -5,26 +5,30 @@
 
 using namespace std;
 
-long long solution(int n, vector<int> times) {
+long long solution(int n, vector<int> times)
+{
 	int tsize = times.size();
 	long long m = *max_element(times.begin(), times.end());
 	long long left = 1, right = m * n;
 	long long mid;
-	while (left <= right) { // left가 더 커지면 종료
+	while (left <= right)
+	{ // left가 더 커지면 종료
 		mid = (left + right) / 2;
 		long long sum = 0;
-		for (int i = 0; i < tsize; i++) 
+		for (int i = 0; i < tsize; i++)
 			sum += (mid / times[i]);
 		if (sum < n)
 			left = mid + 1;
-		else {
+		else
+		{
 			right = mid - 1;
 		}
 	}
 	return left;
 }
 
-int main() {
-	cout << solution(6, { 7, 10 }) << endl;
+int main()
+{
+	cout << solution(6, {7, 10}) << endl;
 	return 0;
 }
