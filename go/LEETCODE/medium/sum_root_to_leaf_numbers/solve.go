cc22b9158dc26b39a7b7e14ea18db5267d7b4ecf
yoon1 <o_ink@naver.com>
2023-05-18 23:53:42 +0900

    go algorithm - LEETCODE: Sum Root to Leaf Numbers

---
+++ go/LEETCODE/medium/sum_root_to_leaf_numbers/solve.go
@@ -0,0 +1,31 @@
+package sum_root_to_leaf_numbers
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sumNumbers(root *TreeNode) int {
+	var (
+		sum = 0
+	)
+
+	var dfs func(node *TreeNode, valPath int)
+	dfs = func(node *TreeNode, valPath int) {
+		valPath *= 10
+		valPath += node.Val
+
+		if node.Left == nil && node.Right == nil {
+			sum += valPath
+			return
+		}
+
+		dfs(node.Left, valPath)
+		dfs(node.Right, valPath)
+	}
+
+	dfs(root, 0)
+
+	return sum
+}
